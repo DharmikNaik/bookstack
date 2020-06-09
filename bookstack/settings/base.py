7d9177f1291df5ad7dbc85eb305002d36fdfc84f
dharmiknaik <dharmiknaik@hotmail.com>
2020-06-09 09:29:23 +0530

    Authentication app initialized, authentication front-end completed

--- bookstack/settings/base.py
+++ bookstack/settings/base.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'authentication',
 ]
 
 MIDDLEWARE = [
@@ -52,7 +53,7 @@ ROOT_URLCONF = 'bookstack.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'authentication/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,3 +117,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIR = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
