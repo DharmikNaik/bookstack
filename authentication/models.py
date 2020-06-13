72dd71fe9011b917f1bc6ab60afabdb212879b9b
dharmiknaik <dharmiknaik@hotmail.com>
2020-06-13 22:57:09 +0530

    Profile model added

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email_address = models.EmailField(blank=False, unique=True)
+    first_name = models.CharField(max_length=50, blank=False)
+    last_name = models.CharField(max_length=50, blank=False)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
--- authentication/views.py
+++ authentication/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
 
 def sign_up(request):
-    return render(request, 'verified.html')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            print(True)
+        else:
+            print(False)
+
+    return render(request, 'signup.html')
